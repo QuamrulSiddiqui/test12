d984e110d428c484fed6684d3df61982341d8cc0
QuamrulSiddiqui <125456749+QuamrulSiddiqui@users.noreply.github.com>
2023-02-28 15:15:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("")
+>>> freecodecamp = "freeCodeCamp"
+
+# Default start and step
+>>> freecodecamp[:8]
+'freeCode'
+
+# Default end and step
+>>> freecodecamp[4:]
+'CodeCamp'
+
+# Default start
+>>> freecodecamp[:8:2]
+'feCd'
+
+# Default stop
+>>> freecodecamp[4::3]
+'Cem'
+
+# Default start and stop
+>>> freecodecamp[::-2]
+'paeoer'
+
+# Default start and stop
+>>> freecodecamp[::-1]
+'pmaCedoCeerf'
